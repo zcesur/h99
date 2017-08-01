e10311fab6aecf89583a0ab92975c49f0eaaed1d
zcesur <zafercesur@gmail.com>
2017-08-01 07:02:01 -0400

    Solve p7

---
+++ p7.hs
@@ -0,0 +1,10 @@
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+main = do
+  print $ flatten (Elem 5)
+  print $ flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
