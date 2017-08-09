799487673f9eeb7fe77d7f4e535a1bc37de85f05
zcesur <zafercesur@gmail.com>
2017-08-09 08:43:51 -0400

    Make function work for all n, m in the domain

--- p24.hs
+++ p24.hs
@@ -1,18 +1,21 @@
 import System.Random
 
--- TO DO:
---
--- 1) Figure out why n = m case does not terminate
--- 2) Handle n > m case
-
 diff_select :: Int -> Int -> IO [Int]
-diff_select n m = do
+diff_select n m
+    | n < 0     = undefined
+    | m < 0     = return []
+    | n > m     = error "Not enough elements in the set"
+    | otherwise = do
     gen <- getStdGen
     let draws = randomRs (1, m) gen
-    return $ last . takeWhile (isSmall n) . scanl addIfDistinct [] $ draws
-      where isSmall n xs = length xs <= n
-            addIfDistinct acc x
-              | x `elem` acc = acc
-              | otherwise    = x:acc
+    return $ last . takeUntil (\x -> length x == n) . scanl addIfDistinct [] $ draws
+  where
+    takeUntil _ []                 = []
+    takeUntil p (x:xs) | p x       = [x]
+                       | otherwise = x : takeUntil p xs
+    
+    addIfDistinct acc x
+        | x `elem` acc = acc
+        | otherwise    = x:acc
 
 main = diff_select 6 49 >>= print
