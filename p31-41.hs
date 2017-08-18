f3c101d01dc04bc696dce392b2cb81a34ff654cc
zcesur <zafercesur@gmail.com>
2017-08-17 23:35:00 -0400

    Solve more problems

--- p31-41.hs
+++ p31-41.hs
@@ -1,5 +1,7 @@
-import Data.List (sort, group)
-import Test.HUnit
+import Data.List (sort, group, find)
+import Data.Maybe (fromJust)
+import Control.Exception.Base (assert)
+import Test.HUnit (assertEqual, runTestTT, Test(..))
 
 -- Problem 31
 -- Determine whether a given integer number is prime.
@@ -104,6 +106,38 @@ primesR a b = filter isPrime [a..b]
 
 test9 = TestCase $ assertEqual "Problem 39" [11,13,17,19] $ primesR 10 20
 
+-- Problem 40
+-- Goldbach's conjecture.
+-- Goldbach's conjecture says that every positive even number greater than 2 is the sum of two prime numbers. Example: 28 = 5 + 23. It is one of the most famous facts in number theory that has not been proved to be correct in the general case. It has been numerically confirmed up to very large numbers (much larger than we can go with our Prolog system). Write a predicate to find the two prime numbers that sum up to a given even integer.
+
+goldbach :: Int -> (Int, Int)
+goldbach n = fromJust $ find (\(x,y) -> isPrime x && isPrime y) candidates
+  where
+    candidates = map (\x -> (x, n'-x)) [1..n' `div` 2]
+    n' = assert (even n && n > 2) n
+
+test10 = TestCase $ assertEqual "Problem 40" (5,23) $ goldbach 28
+
+-- Problem 41
+-- Given a range of integers by its lower and upper limit, print a list of all even numbers and their Goldbach composition.
+-- In most cases, if an even number is written as the sum of two prime numbers, one of them is very small. Very rarely, the primes are both bigger than say 50. Try to find out how many such cases there are in the range 2..3000.
+
+goldbachList :: Int -> Int -> [(Int, Int)]
+goldbachList a b = map goldbach [x | x <- [a..b], even x, x > 2]
+
+goldbachList' :: Int -> Int -> Int -> [(Int, Int)]
+goldbachList' a b c = filter (\(x,y) -> x > c && y > c) $ goldbachList a b
+
+test11a = TestCase $ assertEqual "Problem 41" expected actual
+  where
+    expected = [(3,7),(5,7),(3,11),(3,13),(5,13),(3,17)]
+    actual = goldbachList 9 20
+
+test11b = TestCase $ assertEqual "Problem 41" expected actual
+  where
+    expected = [(73,919),(61,1321),(67,1789),(61,1867)]
+    actual = goldbachList' 4 2000 50
+
 main = do
     let tests = TestList
             [ test1a 
@@ -114,6 +148,9 @@ main = do
             , test5
             , test6
             , test7
-            , test9 ]
+            , test9
+            , test10
+            , test11a
+            , test11b ]
 
     runTestTT tests 
